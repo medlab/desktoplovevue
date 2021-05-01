f456d8dba3ed52ee4e82089832909187912973e2
Cong Zhang <congzhangzh@gmail.com>
2021-05-01 22:00:39 +0800

    make cppdemo optional

--- start.py
+++ start.py
@@ -4,36 +4,6 @@ import webview
 import logging
 import threading
 
-#--begin-- cppyy test region begion
-import cppyy
-
-cppyy.cppdef("""
-class MyClass {
-public:
-    MyClass(int i) : m_data(i) {}
-    virtual ~MyClass() {}
-    virtual int add_int(int i) { return m_data + i; }
-    int m_data;
-};""")
-
-from cppyy.gbl import MyClass
-
-c=MyClass(42)
-
-cppyy.cppdef("""
- void say_hello(MyClass* m) {
-     std::cout << "Hello, the number is: " << m->m_data << std::endl;
- }""")
-
-MyClass.say_hello=cppyy.gbl.say_hello
-
-logging.warn('hello world' * 80)
-for i in range(10):
-    c.add_int(3)
-    c.say_hello()
-
-#--end-- cppyy test region begion
-
 class Api():
     def addItem(self, title):
         print('Added item %s' % title)
@@ -51,6 +21,35 @@ class Api():
         webview.windows[0].toggle_fullscreen()
 
     def cppyyDemo(self, msg):
+        #--begin-- cppyy test region begion
+        import cppyy
+
+        cppyy.cppdef("""
+        class MyClass {
+        public:
+            MyClass(int i) : m_data(i) {}
+            virtual ~MyClass() {}
+            virtual int add_int(int i) { return m_data + i; }
+            int m_data;
+        };""")
+
+        from cppyy.gbl import MyClass
+
+        c=MyClass(42)
+
+        cppyy.cppdef("""
+        void say_hello(MyClass* m) {
+            std::cout << "Hello, the number is: " << m->m_data << std::endl;
+        }""")
+
+        MyClass.say_hello=cppyy.gbl.say_hello
+
+        logging.warn('hello world' * 80)
+        for i in range(10):
+            c.add_int(3)
+            c.say_hello()
+
+        #--end-- cppyy test region begion
         logging.warn('--begin-- to call cppyyDemo'*80+msg)
         c.say_hello()
         logging.warn('--end-- to call cppyyDemo'*80+msg)
@@ -73,6 +72,5 @@ if __name__ == '__main__':
 
     #debug will enable right click menu
     #http_server will enable a http server, which works better than files:/ protocol
-    #cef engine is better to works for all os
+    #cef engine works better for all os
     webview.start(debug=True, http_server=True, gui='cef')
-
